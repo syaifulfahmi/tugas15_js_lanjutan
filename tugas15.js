d42dba6f9208101f46efe115f4567d0bdfd7b277
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-09-21 10:18:27 +0700

    Add files via upload

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+var AB = 8
+var BC = 6
+var hasil;
+
+console.log("Nilai AB : 8 cm")
+console.log("Nilai BC : 6 cm")
+
+rumusSegitiga = (Math.pow(8,2)) + (Math.pow(6,2))
+hasil = (Math.sqrt(rumusSegitiga))
+
+console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah: ", hasil, "cm")
